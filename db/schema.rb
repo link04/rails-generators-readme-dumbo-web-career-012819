9334b7f37df2bf3e37c7cfa1705bf5b8211d185b
Maximo Bautista <max.bautista97@gmail.com>
2019-02-27 01:00:13 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190227005819) do
+ActiveRecord::Schema.define(version: 20190227010000) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
     t.string   "genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "bio"
   end
 
   create_table "categories", force: :cascade do |t|
